9d5205231d349593ab9e646820d645bf96c6968d
amanmaurya0704 <aman.maurya0704@gmail.com>
2024-02-19 22:06:47 +0530

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r",encoding="utf-8") as f:
+    long_description = f.read()
+    
+    
+__version__ = "0.0.0"
+
+REPO_NAME = "textsummarizer_project"
+Author_USER_NAME = "amanmaurya0704"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "aman.maurya0704@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=Author_USER_NAME,
+    author_email= AUTHOR_EMAIL,
+    description="Text Summarizer: a small package for NLP App",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{Author_USER_NAME}/{REPO_NAME}",
+    project_urls = {"Bug Tracker": f"https://github.com/{Author_USER_NAME}/{REPO_NAME}/issues"},
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+    )
