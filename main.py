42c2669264482fed5259eb49e31f2ab964fc01eb
amanmaurya0704 <aman.maurya0704@gmail.com>
2024-02-21 09:17:08 +0530

    Data Ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from textSummarizer.pipeline.stage_01_dataingestion import DataIngestionTrainningPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcome to our custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = DataIngestionTrainningPipeline()
+    data_ingestion.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<<<<<\n\nx=========================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
